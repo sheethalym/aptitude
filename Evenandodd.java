a40ab475d07936e6f6520402293699101fc820f5
sheethalym <sheethal.xworkz@gmail.com>
2021-12-23 23:11:26 +0530

    adding aptitude

---
+++ Evenandodd.java
@@ -0,0 +1,12 @@
+class Evenandodd{
+ public static void main(String[] args){
+  for(int num =1;num<20;num= num+1){
+   if(num %2==0){
+  System.out.println(num + " is even");
+   }else{
+  System.out.println(num + " is odd");
+   }
+  }
+ }
+
+}
---
+++ Evennum.java
@@ -0,0 +1,11 @@
+class Evennum{
+ public static void main(String[] args){
+ int num = 19;
+ if(num %2==0){
+  System.out.println("even number");
+   }else{
+  System.out.println("odd number");
+  }
+ }
+
+}
